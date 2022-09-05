6a1f93196b953f27eea82e4f0e03a52ceee98d14
Christian <onovochristian19@gmail.com>
2022-09-05 22:17:33 +0100

    =forgot password inplementation

--- forgot.php
+++ forgot.php
@@ -32,90 +32,76 @@
     }
     /* DB Connection Closing */
 
-    if (isset($_POST['submit'])) {
-    
-      echo "  <html>
-                  <script>
-                     window.alert('Check Email to change ypur password');
-                  </script>
-            </html>
-         "; 
-         
-         if(isset($_POST["email"]) && (!empty($_POST["email"]))){
-            $email = $_POST["email"];
-            $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-            $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-            if (!$email) {
-               $error .="<p>Invalid email address please type a valid email address!</p>";
-               }else{
-               $sel_query = "SELECT * FROM `users` WHERE email='".$email."'";
-               $results = mysqli_query($con,$sel_query);
-               $row = mysqli_num_rows($results);
-               if ($row==""){
-               $error .= "<p>No user is registered with this email address!</p>";
-               }
-              }
-               if($error!=""){
-               echo "<div class='error'>".$error."</div>
-               <br /><a href='javascript:history.go(-1)'>Go Back</a>";
-               }else{
-               $expFormat = mktime(
-               date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
-               );
-               $expDate = date("Y-m-d H:i:s",$expFormat);
-               $key = md5(2418*2+$email);
-               $addKey = substr(md5(uniqid(rand(),1)),3,10);
-               $key = $key . $addKey;
-            // Insert Temp Table
-            mysqli_query($con,
-            "INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
-            VALUES ('".$email."', '".$key."', '".$expDate."');");
-            
-            $output='<p>Dear user,</p>';
-            $output.='<p>Please click on the following link to reset your password.</p>';
-            $output.='<p>-------------------------------------------------------------</p>';
-            $output.='<p><a href="https://www.allphptricks.com/forgot-password/reset-password.php?
-            key='.$key.'&email='.$email.'&action=reset" target="_blank">
-            https://www.allphptricks.com/forgot-password/reset-password.php
-            ?key='.$key.'&email='.$email.'&action=reset</a></p>';		
-            $output.='<p>-------------------------------------------------------------</p>';
-            $output.='<p>Please be sure to copy the entire link into your browser.
-            The link will expire after 1 day for security reason.</p>';
-            $output.='<p>If you did not request this forgotten password email, no action 
-            is needed, your password will not be reset. However, you may want to log into 
-            your account and change your security password as someone may have guessed it.</p>';   	
-            $output.='<p>Thanks,</p>';
-            $output.='<p>AllPHPTricks Team</p>';
-            $body = $output; 
-            $subject = "Password Recovery - AllPHPTricks.com";
-            
-            $email_to = $email;
-            $fromserver = "noreply@yourwebsite.com"; 
-            require("PHPMailer/PHPMailerAutoload.php");
-            $mail = new PHPMailer();
-            $mail->IsSMTP();
-            $mail->Host = "mail.yourwebsite.com"; // Enter your host here
-            $mail->SMTPAuth = true;
-            $mail->Username = "noreply@yourwebsite.com"; // Enter your email here
-            $mail->Password = "password"; //Enter your password here
-            $mail->Port = 25;
-            $mail->IsHTML(true);
-            $mail->From = "noreply@yourwebsite.com";
-            $mail->FromName = "AllPHPTricks";
-            $mail->Sender = $fromserver; // indicates ReturnPath header
-            $mail->Subject = $subject;
-            $mail->Body = $body;
-            $mail->AddAddress($email_to);
-            if(!$mail->Send()){
-            echo "Mailer Error: " . $mail->ErrorInfo;
-            }else{
-            echo "<div class='error'>
-            <p>An email has been sent to you with instructions on how to reset your password.</p>
-            </div><br /><br /><br />";
-               }
-               }
-            }else{
-    }
+    if(isset($_POST["email"]) && (!empty($_POST["email"]))){
+      $email = $_POST["email"];
+      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+      $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+      if (!$email) {
+         $error .="<p>Invalid email address please type a valid email address!</p>";
+      }else{
+         $sel_query = "SELECT * FROM `create_acc` WHERE email_id='".$email."'";
+         $results = mysqli_query($con,$sel_query);
+         $row = mysqli_num_rows($results);
+         if ($row==""){
+         $error .= "<p>No user is registered with this email address!</p>";
+         }
+      }
+      if($error!=""){
+         echo "<div class='error'>".$error."</div>
+         <br /><a href='javascript:history.go(-1)'>Go Back</a>";
+      }else{
+         $expFormat = mktime(
+         date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
+         );
+         $expDate = date("Y-m-d H:i:s",$expFormat);
+         $key = md5(2418*2+$email);
+         $addKey = substr(md5(uniqid(rand(),1)),3,10);
+         $key = $key . $addKey;
+      // Insert Temp Table
+      mysqli_query($con,
+      "INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
+      VALUES ('".$email."', '".$key."', '".$expDate."');");
+      
+      $output='<p>Dear user,</p>';
+      $output.='<p>Please click on the following link to reset your password.</p>';
+      $output.='<p>-------------------------------------------------------------</p>';
+      $output.='<p><a href="https://www.binancevest.com/reset-password.php?
+      key='.$key.'&email='.$email.'&action=reset" target="_blank">
+      https://www.binancevest.com/reset-password.php
+      ?key='.$key.'&email='.$email.'&action=reset</a></p>';		
+      $output.='<p>-------------------------------------------------------------</p>';
+      $output.='<p>Please be sure to copy the entire link into your browser.
+      The link will expire after 1 day for security reason.</p>';
+      $output.='<p>If you did not request this forgotten password email, no action 
+      is needed, your password will not be reset. However, you may want to log into 
+      your account and change your security password as someone may have guessed it.</p>';   	
+      $output.='<p>Thanks,</p>';
+      $output.='<p>BinanceVest Team</p>';
+      $body = $output; 
+      $subject = "Password Recovery - BinanceVest.com";
+      
+      $to = $_POST["email"];
+      $subject = "Binancevestltd Signup";
+      $email_content = $body;
+      $message = $email_content;
+
+      
+      $header = "From:support@binancvestltd.co \r\n";
+      $header .= "Cc:afgh@somedomain.com \r\n";
+      $header .= "MIME-Version: 1.0\r\n";
+      $header .= "Content-type: text/html\r\n";
+      
+      $retval = mail ($to,$subject,$message,$header);
+
+      if (retval == false){
+         echo "Error in sending email";
+      }else{
+         echo "<div class='error'>
+         <p>An email has been sent to you with instructions on how to reset your password.</p>
+         </div><br /><br /><br />";
+      }
+         }
+   }else{
 ?>
 
 <!DOCTYPE html>
@@ -398,7 +384,7 @@
             <!--<div id="wait_loader" class="progress progress-striped active">-->
             <!--    <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>-->
             <!--</div>-->
-            <input type="submit" name="submit" value="Submit" class="full-width">
+            <input type="submit" value="Reset Password" class="full-width">
          </form>
       </div>
 
@@ -434,4 +420,4 @@
 
 </html>
 
-<?php mysqli_close($conn); ?> 
+<?php } ?> 
