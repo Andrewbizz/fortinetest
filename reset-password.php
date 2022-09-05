730db6a3673eda3605045ad8c4ffb7cda70eb46f
Christian <onovochristian19@gmail.com>
2022-09-05 23:26:42 +0100

    =reset password functionality

--- reset-password.php
+++ reset-password.php
@@ -1,87 +1,109 @@
 <?php
-include('db.php');
-if(isset($_POST["email"]) && (!empty($_POST["email"]))){
-$email = $_POST["email"];
-$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$email = filter_var($email, FILTER_VALIDATE_EMAIL);
-if (!$email) {
-   $error .="<p>Invalid email address please type a valid email address!</p>";
-   }else{
-   $sel_query = "SELECT * FROM `users` WHERE email='".$email."'";
-   $results = mysqli_query($con,$sel_query);
-   $row = mysqli_num_rows($results);
-   if ($row==""){
-   $error .= "<p>No user is registered with this email address!</p>";
+
+   
+   session_start();
+
+   include("./include_files/functions.php");
+   include("./include_files/validate_functions.php");
+
+   // Declaring and hoisting the variables
+   $username = "";
+   $email    = "";
+   $errors = array();
+   $_SESSION['success'] = "";
+
+   /* Production DB Connection */
+   //    $db_host = "localhost";
+   //    $db_user = "binaqfcu_binancevestu1";
+   //    $db_pass = "@mikord09";
+   //    $db_name = "binaqfcu_binanceVest-db";
+
+   /* Development DB Connection */
+   $db_host = "localhost";
+   $db_user = "root";
+   $db_pass = "mikord";
+   $db_name = "binaqfcu_binanceVest-db";
+
+   // Create connection
+   $conn = mysqli_connect($db_host, $db_user, $db_pass,$db_name);
+
+   // Check connection
+   if (!$conn){
+      die("Connection failed: " . mysqli_connect_error());
    }
-  }
-   if($error!=""){
-   echo "<div class='error'>".$error."</div>
-   <br /><a href='javascript:history.go(-1)'>Go Back</a>";
-   }else{
-   $expFormat = mktime(
-   date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
+   /* DB Connection Closing */
+
+   if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) 
+   && ($_GET["action"]=="reset") && !isset($_POST["action"])){
+   $key = $_GET["key"];
+   $email = $_GET["email"];
+   $curDate = date("Y-m-d H:i:s");
+   $query = mysqli_query($con,
+   "SELECT * FROM `password_reset_temp` WHERE `key`='".$key."' and `email`='".$email."';"
    );
-   $expDate = date("Y-m-d H:i:s",$expFormat);
-   $key = md5(2418*2+$email);
-   $addKey = substr(md5(uniqid(rand(),1)),3,10);
-   $key = $key . $addKey;
-// Insert Temp Table
-mysqli_query($con,
-"INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
-VALUES ('".$email."', '".$key."', '".$expDate."');");
+   $row = mysqli_num_rows($query);
+   if ($row==""){
+   $error .= '<h2>Invalid Link</h2>
+   <p>The link is invalid/expired. Either you did not copy the correct link
+   from the email, or you have already used the key in which case it is 
+   deactivated.</p>
+   <p><a href="https://www.binancvestltd.co/forgot.php">
+   Click here</a> to reset password.</p>';
+      }else{
+   $row = mysqli_fetch_assoc($query);
+   $expDate = $row['expDate'];
+   if ($expDate >= $curDate){
+   ?>
+   <br />
+   <form method="post" action="" name="update">
+   <input type="hidden" name="action" value="update" />
+   <br /><br />
+   <label><strong>Enter New Password:</strong></label><br />
+   <input type="password" name="pass1" maxlength="15" required />
+   <br /><br />
+   <label><strong>Re-Enter New Password:</strong></label><br />
+   <input type="password" name="pass2" maxlength="15" required/>
+   <br /><br />
+   <input type="hidden" name="email" value="<?php echo $email;?>"/>
+   <input type="submit" value="Reset Password" />
+   </form>
+   <?php
+   }else{
+   $error .= "<h2>Link Expired</h2>
+   <p>The link is expired. You are trying to use the expired link which 
+   as valid only 24 hours (1 days after request).<br /><br /></p>";
+               }
+         }
+   if($error!=""){
+   echo "<div class='error'>".$error."</div><br />";
+   }			
+   } // isset email key validate end
 
-$output='<p>Dear user,</p>';
-$output.='<p>Please click on the following link to reset your password.</p>';
-$output.='<p>-------------------------------------------------------------</p>';
-$output.='<p><a href="https://www.allphptricks.com/forgot-password/reset-password.php?
-key='.$key.'&email='.$email.'&action=reset" target="_blank">
-https://www.allphptricks.com/forgot-password/reset-password.php
-?key='.$key.'&email='.$email.'&action=reset</a></p>';		
-$output.='<p>-------------------------------------------------------------</p>';
-$output.='<p>Please be sure to copy the entire link into your browser.
-The link will expire after 1 day for security reason.</p>';
-$output.='<p>If you did not request this forgotten password email, no action 
-is needed, your password will not be reset. However, you may want to log into 
-your account and change your security password as someone may have guessed it.</p>';   	
-$output.='<p>Thanks,</p>';
-$output.='<p>AllPHPTricks Team</p>';
-$body = $output; 
-$subject = "Password Recovery - AllPHPTricks.com";
 
-$email_to = $email;
-$fromserver = "noreply@yourwebsite.com"; 
-require("PHPMailer/PHPMailerAutoload.php");
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Host = "mail.yourwebsite.com"; // Enter your host here
-$mail->SMTPAuth = true;
-$mail->Username = "noreply@yourwebsite.com"; // Enter your email here
-$mail->Password = "password"; //Enter your password here
-$mail->Port = 25;
-$mail->IsHTML(true);
-$mail->From = "noreply@yourwebsite.com";
-$mail->FromName = "AllPHPTricks";
-$mail->Sender = $fromserver; // indicates ReturnPath header
-$mail->Subject = $subject;
-$mail->Body = $body;
-$mail->AddAddress($email_to);
-if(!$mail->Send()){
-echo "Mailer Error: " . $mail->ErrorInfo;
-}else{
-echo "<div class='error'>
-<p>An email has been sent to you with instructions on how to reset your password.</p>
-</div><br /><br /><br />";
-	}
+   if(isset($_POST["email"]) && isset($_POST["action"]) &&
+   ($_POST["action"]=="update")){
+   $error="";
+   $pass1 = mysqli_real_escape_string($con,$_POST["pass1"]);
+   $pass2 = mysqli_real_escape_string($con,$_POST["pass2"]);
+   $email = $_POST["email"];
+   $curDate = date("Y-m-d H:i:s");
+   if ($pass1!=$pass2){
+   $error.= "<p>Password do not match, both password should be same.<br /><br /></p>";
    }
-}else{
+   if($error!=""){
+   echo "<div class='error'>".$error."</div><br />";
+   }else{
+   $pass1 = md5($pass1);
+   mysqli_query($con,
+   "UPDATE `create_acc` SET `passwrd`='".$pass1."', `trn_date`='".$curDate."' 
+   WHERE `email`='".$email."';"
+   );
+
+   mysqli_query($con,"DELETE FROM `password_reset_temp` WHERE `email`='".$email."';");
+      
+   echo '<div class="error"><p>Congratulations! Your password has been updated successfully.</p>
+   <p><a href="https://www.allphptricks.com/forgot-password/login.php">
+   Click here</a> to Login.</p></div><br />';
+      }		
+   }
 ?>
-<form method="post" action="" name="reset"><br /><br />
-<label><strong>Enter Your Email Address:</strong></label><br /><br />
-<input type="email" name="email" placeholder="username@email.com" />
-<br /><br />
-<input type="submit" value="Reset Password"/>
-</form>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<?php } ?>
